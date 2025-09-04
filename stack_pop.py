506914c738402c1cc7049082a33d166bf75a3423
Vivaan <173051828+VivaanGupta2509@users.noreply.github.com>
2025-09-04 11:11:53 +0530

    Add files via upload

---
+++ stack_pop.py
@@ -0,0 +1,12 @@
+# Simple stack pop example
+
+# Initial stack
+stack = [10, 20, 30, 40]
+
+print("Initial stack:", stack)
+
+# Pop elements one by one
+print("Popped:", stack.pop())   # removes 40
+print("Popped:", stack.pop())   # removes 30
+
+print("Stack after popping:", stack)
